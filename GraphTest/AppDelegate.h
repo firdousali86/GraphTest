f7fb800305329739898c909306f183c235397057
Firdous Ali <firdousali@firdouss-mac-mini.local>
2015-12-30 21:07:05 +0500

    Initial Commit

---
+++ GraphTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GraphTest
+//
+//  Created by Firdous Ali on 30/12/2015.
+//  Copyright © 2015 Firdous Ali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GraphTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GraphTest
+//
+//  Created by Firdous Ali on 30/12/2015.
+//  Copyright © 2015 Firdous Ali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
